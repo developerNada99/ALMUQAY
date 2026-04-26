4f9a4d942b80f8360d41eabb1843c056635fac7b
NadaAhme <nadaahmed33470@gmail.com>
2026-04-26 07:29:03 +0300

    add ed

--- app/(links)/about/page.tsx
+++ app/(links)/about/page.tsx
@@ -45,7 +45,7 @@ const About = () => {
               <div className="absolute top-0 right-0 w-[40%] aspect-square 
                               rounded-2xl overflow-hidden shadow-lg rotate-6">
                 <Image
-                  src="/about.jfif"
+                  src="/buy.jfif"
                   alt="عقار"
                   fill
                   priority
